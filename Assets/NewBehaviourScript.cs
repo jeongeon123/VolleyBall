6660f45f4b56af36193cc1942f4ddac12db6cb87
jeongeon123 <i1100i@icloud.com>
2023-10-26 23:43:35 +0900

    231020

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -4,11 +4,24 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+    public float maxspeed;
     Rigidbody2D rigid;
+    SpriteRenderer spriteRenderer;
 
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+    private void Update()
+    {
+        if (Input.GetButtonUp("Horizontal"))
+        {
+            rigid.velocity = new Vector2(rigid.velocity.normalized.x * 0.5f, rigid.velocity.y);
+        }
+        if (Input.GetButtonDown("Horizontal"))
+            spriteRenderer.flipX = Input.GetAxisRaw("Horizontal") == -1;
+           
     }
     void FixedUpdate()
     {
@@ -18,6 +31,7 @@ public class NewBehaviourScript : MonoBehaviour
         if (rigid.velocity.x > maxspeed)
             rigid.velocity = new Vector2(maxspeed, rigid.velocity.y);
         else if (rigid.velocity.x < maxspeed*(-1))
-            rigid.velocity = new Vector2(maxspeed, rigid.velocity.y);
+            rigid.velocity = new Vector2(maxspeed*(-1), rigid.velocity.y);
     }
 }
+
